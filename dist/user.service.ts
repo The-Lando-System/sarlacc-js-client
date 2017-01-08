ad21f4a9e128b8dd9356fdae8531de3ba1aaab24
Matt <matt.voget@gmail.com>
2017-01-08 15:30:00 -0700

    actually ready for use

--- dist/user.service.ts
+++ dist/user.service.ts
@@ -1,10 +1,9 @@
-import { Injectable, EventEmitter } from '@angular/core';
-import { Http, Headers, RequestOptions } from '@angular/http';
-import { Observable } from 'rxjs/Rx';
-import 'rxjs/add/operator/map';
+import { Injectable } from '@angular/core';
+import { Http, Headers } from '@angular/http';
+import { CookieService } from 'angular2-cookie/services/cookies.service';
+import { Logger } from "angular2-logger/core";
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/operator/toPromise';
-import { CookieService } from 'angular2-cookie/services/cookies.service';
 
 import { Broadcaster } from './broadcaster';
 import { User } from './user';
@@ -13,8 +12,8 @@ import { Token } from './token';
 @Injectable()
 export class UserService {
 
-  //private tokenUrl = 'http://localhost:8080/oauth/token';
-  //private userUrl = 'http://localhost:8080/user-details';
+  private TAG = 'UserService - ';
+
   private tokenUrl = 'https://sarlacc.herokuapp.com/oauth/token';
   private userUrl = 'https://sarlacc.herokuapp.com/user-details';
 
@@ -22,68 +21,91 @@ export class UserService {
   private user: User;
 
   private initPromise: Promise<any> = this.initialize();
+
+  public LOGIN_BCAST = 'LOGIN';
+  public LOGOUT_BCAST = 'LOGOUT';
   
   constructor(
+    private logger: Logger,
     private http: Http,
     private cookieService: CookieService,
     private broadcaster: Broadcaster
   ){}
 
-  initialize(): Promise<any> {
+  // Initialization ================================================
+
+  private initialize(): Promise<any> {
+
+    this.logger.info(this.TAG + 'Initializing the user service');
+
     return new Promise((resolve, reject) => {
-      console.log("UserSvc Init - Initializing user and token");
+
       this.token = this.getTokenFromCookie();
+
       if (this.token) {
-        console.log("UserSvc Init - Found a token in the cookies. Retrieving the user");
+        this.logger.debug(this.TAG + 'Attempting to get user details with the access token');
         this.retrieveUser(this.token)
         .then((user:User) => {
+          this.logger.info(this.TAG + 'Intialization completed... User and token are both set');
           this.user = user;
-          console.log("UserSvc Init - Found a user. Both user and token are initialized");
           resolve();
         }).catch((error:any) => {
-          console.log("UserSvc Init - Failed to retrieve the user. Error below");
-          console.log(error);
+          this.logout();
+          this.logger.info(this.TAG + 'Initialization complete. No user logged in');
           reject();
         });
       } else {
-        console.log("UserSvc Init - No token detected. Both user and token are null. User needs to login");
+        this.logger.info(this.TAG + 'Initialization complete. No user logged in');
         reject();
       }
     });
   }
 
+  // Public Methods ================================================
+
   checkIfUserIsLoggedIn(): Promise<any> {
     return this.initPromise;
   }
 
-  getAuthHeaders(): Headers {
-    return new Headers({
-      'Content-Type'   : 'application/json',
-      'x-access-token'  : this.getToken().access_token
+  login(creds: any): Promise<User> {
+    this.logger.info(this.TAG + 'User logging in with username ' + creds.username);
+    this.logger.debug(this.TAG + 'Attempting to obtain an access token');
+
+    return new Promise((resolve, reject) => {
+
+      this.retrieveToken(creds)
+      .then((token:Token) => {
+
+        this.putTokenInCookie(token);
+        this.token = token;
+
+        this.logger.debug(this.TAG + 'Attempting to get user details with the access token');
+        this.retrieveUser(token)
+        .then((user:User) => {
+          this.logger.info(this.TAG + 'Login successful');
+          this.user = user;
+          this.broadcastLogin('User ' + user.username + ' has logged in!');
+          resolve(user);
+        }).catch((error:any) => {
+          this.logger.info(this.TAG + 'Login failed');
+          this.logout();
+          reject(error);
+        });
+      }).catch((error:any) => {
+        this.logger.info(this.TAG + 'Login failed');
+        this.logout();
+        reject(error);
+      });
+
     });
   }
 
-  login(creds: any): Promise<User> {
-    console.log("login - logging in with username " + creds.username + " and password " + creds.password);
-    return this.retrieveToken(creds)
-    .then((token:Token) => {
-      this.putTokenInCookie(token);
-      this.token = token;
-      console.log("login - successfully got token and put it in the cookies");
-      return this.retrieveUser(token)
-      .then((user:User) => {
-        this.user = user;
-        console.log("login - successfully got user");
-        this.broadcaster.broadcast('Login','The user logged in');
-        return user;
-      }).catch((error:any) => {
-        console.log("login - error retrieving the user. Details below:");
-        console.log(error);
-      });
-    }).catch((error:any) => {
-      console.log("login - Error retrieving the token. Details below:");
-      console.log(error);
-    });
+  logout(): void {
+    this.logger.info(this.TAG + 'Logging the user out');
+    this.removeTokenFromCookie();
+    this.token = null;
+    this.user = null;
+    this.broadcastLogout('User has logged out!');
   }
 
   getUser(): User {
@@ -94,57 +116,111 @@ export class UserService {
     return this.token;
   }
 
-  retrieveUser(token:Token): Promise<User> {
-    return this.http.post(this.userUrl, {}, {headers: this.getUserHeaders(token.access_token)})
-      .toPromise()
-      .then((res:any) => {
-        return res.json();
-      }).catch((res:any) => {
-        return res.json();
-      });
+  getAuthHeaders(): Headers {
+    return new Headers({
+      'Content-Type'   : 'application/json',
+      'x-access-token'  : this.getToken().access_token
+    });
   }
 
-  retrieveToken(creds:any): Promise<Token> {
+  
+  // Private Methods ================================================
+
+  private retrieveUser(token:Token): Promise<User> {
+    return new Promise((resolve, reject) => {
+      this.http.post(this.userUrl, {}, {headers: this.getUserHeaders(token.access_token)})
+        .toPromise()
+        .then((res:any) => {
+          var user = res.json();
+          this.logger.debug(this.TAG + 'Successfully got a user with username: ' + user.username);
+          this.logger.debug(this.TAG + 'Full user details: ' + JSON.stringify(user));
+          resolve(user);
+        }).catch((res:any) => {
+          var error = res.json();
+          this.logger.warn(this.TAG + 'Failed to retrieve a user with access token: ' + token.access_token);
+          this.logger.debug(this.TAG + 'Error from the server: ' + JSON.stringify(error));
+          reject(this.resolveError(error));
+        });
+    });
+  }
+
+  private retrieveToken(creds:any): Promise<Token> {
 
     creds.grant_type = 'password';
     let body = `username=${creds.username}&password=${creds.password}&grant_type=${creds.grant_type}`;
 
-    return this.http.post(this.tokenUrl, body, {headers: this.getTokenHeaders()})
-    .toPromise()
-    .then((res:any) => {
-      return res.json();
-    }).catch((res:any) => {
-      return res.json();
+    return new Promise((resolve, reject) => {
+      this.http.post(this.tokenUrl, body, {headers: this.getTokenHeaders()})
+      .toPromise()
+      .then((res:any) => {
+        var token = res.json();
+        this.logger.debug(this.TAG + 'Successfully retrieved an access token: ' + token.access_token);
+        this.logger.debug(this.TAG + 'Full token details: ' + JSON.stringify(token));
+        resolve(token);
+      }).catch((res:any) => {
+        var error = res.json();
+        this.logger.warn(this.TAG + 'Failed to retrieve an access token');
+        this.logger.debug(this.TAG + 'Error from the server: ' + JSON.stringify(error));
+        reject(this.resolveError(error));
+      });
     });
   }
 
-  putTokenInCookie(token:Token): void {
+  private resolveError(error:any): string {
+    var message = 'Unknown error occurred';
+    if (error.status === 404){
+      message = 'Failed to connect to the Sarlacc';
+    }
+    if (error.error === 'invalid_token') {
+      message = 'Invalid or bad token provided';
+    }
+    if (error.error === 'invalid_grant' || error.error === 'unauthorized'){
+      message = 'Incorrect username or password';
+    }
+    this.logger.debug(this.TAG + 'Resolved error: ' + message);
+    return message;
+  }
+
+  private broadcastLogin(message:string): void {
+    this.broadcaster.broadcast(this.LOGIN_BCAST,message);
+  }
+
+  private broadcastLogout(message:string): void {
+    this.broadcaster.broadcast(this.LOGOUT_BCAST,message);
+  }
+
+  private putTokenInCookie(token:Token): void {
+    this.logger.debug(this.TAG + 'Putting the following value in the access-token cookie: ' + token.access_token);
     this.cookieService.put('access-token',token.access_token);
   }
 
-  getTokenFromCookie(): Token {
+  private getTokenFromCookie(): Token {
+    this.logger.debug(this.TAG + 'Checking if the browser has the access-token cookie');
     var access_token = this.cookieService.get('access-token');
     if (access_token) {
+      this.logger.debug(this.TAG + 'Found the following value in the access-token cookie: ' + access_token);
       var token: Token = new Token();
       token.access_token = access_token;
       return token;
     } else {
+      this.logger.debug(this.TAG + 'No access-token cookie found');
       return null;
     }
   }
 
-  removeTokenFromCookie(): void {
+  private removeTokenFromCookie(): void {
+    this.logger.debug(this.TAG + 'Removing the access-token cookie');
     this.cookieService.remove('access-token');
   }
 
-  getTokenHeaders(): Headers {
+  private getTokenHeaders(): Headers {
     return new Headers({
       'Content-Type'   : 'application/x-www-form-urlencoded',
       'Authorization'  : 'Basic ' + btoa('sarlacc:deywannawanga')
     });
   }
 
-  getUserHeaders(token:string): Headers {
+  private getUserHeaders(token:string): Headers {
     return new Headers({
       'Authorization'  : 'Bearer ' + token
     });
